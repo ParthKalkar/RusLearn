4a7c39791509996c0d8a82ce964b48ea07e4d86a
RafikHachana <rafikhachana@gmail.com>
2021-09-19 22:37:31 +0300

    Fix attempt for WSGI

--- ruslearn/wsgi.py
+++ ruslearn/wsgi.py
@@ -7,7 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+# # add the hellodjango project path into the sys.path
+# sys.path.append('ruslearn')
+#
+# # add the virtualenv site-packages path to the sys.path
+sys.path.append('venv/lib/python3.8/site-packages')
+
+
 
 from django.core.wsgi import get_wsgi_application
 
