79c90c0ed33ce1ee01aa8449f9b5bb77c868bf18
RafikHachana <rafikhachana@gmail.com>
2021-09-28 15:23:14 +0300

    Add login and signup HTML templates

--- main_app/views.py
+++ main_app/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'welcome.html')
     # return HttpResponse('Hello, World!')
 
 
